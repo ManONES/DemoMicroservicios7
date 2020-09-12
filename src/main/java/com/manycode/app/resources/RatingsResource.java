4f45666d9f6789229db84a0e585e9ad46d4d3eb4
ManONES <manuel.romero@siessoftware.net>
2020-09-11 19:01:47 -0500

    Our F C

--- src/main/java/com/manycode/app/resources/RatingsResource.java
+++ src/main/java/com/manycode/app/resources/RatingsResource.java
@@ -1,6 +1,9 @@
 package com.manycode.app.resources;
 
 import com.manycode.app.model.UserRating;
+
+import java.util.Random;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +17,17 @@ public class RatingsResource {
 
     @RequestMapping("/user/{userId}")
     public UserRating getUserRatings(@PathVariable("userId") String userId) {
+    	int iii = 0;      
+		for (int ii = 0; ii < 10; ii++) {
+	        while (true) {
+			    iii =getRandomNumberInRange(1, 20000);
+	            if (iii==1974) { break;}
+	        }
+		}        	
+    	
+    	
         UserRating userRating = new UserRating();
-        userRating.initData(userId);
+        userRating.initData(userId + ":" +iii);
         return userRating;
 
     }
@@ -33,6 +45,13 @@ public class RatingsResource {
     @GetMapping(value ="/testdestino")
 	public String welcome() {
 		return "Hola al mundo de Many  con  microservicios 1841";
+	}	
+    
+	private static int getRandomNumberInRange(int min, int max) {
+		
+		Random r = new Random();
+		return r.ints(min, (max + 1)).limit(1).findFirst().getAsInt();
+		
 	}	
     
     
